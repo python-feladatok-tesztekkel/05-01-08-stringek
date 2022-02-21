89d20adbd9969170232b079e5f7fdda1766376d1
Csaba Gyuris <gycsaba@vasvari.hu>
2022-02-21 09:41:41 +0100

    Feladat és tesztek

--- feladatok.py
+++ feladatok.py
@@ -1,5 +1,25 @@
 # feladat.py
 
 # 1. feladat
-def feladat01() -> int:
-    return 0
+# Írjon függvényt ebetuk_szama néven
+# A függvény határozza meg a paraméterben kapott stringben hány "e" betű van!
+
+def ebetuk_szama(szo:str) -> int:
+    db = 0
+    for betu in szo:
+
+# 2. feladat
+# Írjon függvényt szavak_szama néven
+# Határozza meg, hogy a paramérerben kapott mondatban hány darab szó
+
+# 3. feladat
+# Írjon függvényt harom_betus_szavak néven
+# A föggvény paraméterben egy mondatot kap.
+# A függvyén határozza meg a mondat lévő három betűs szavakat
+# A karakterláncban csak betűk és szóközök vannak, írásjelek nélkül
+
+# 4. feladat
+# Írjon függvényt legrovidebb_szo néven
+# A függvény határzza meg a legrövidebb szó hosszát a paraméterben kapott mondatba.
+
+
--- teszt/test_feladat01.py
+++ teszt/test_feladat01.py
@@ -6,8 +6,24 @@ parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir)
 import feladatok
 
-class TestOsszeg(TestCase):
+class TestEBetukSzama(TestCase):
     def test_feladat01(self):
-        aktualis = feladatok.feladat01()
+        aktualis = feladatok.ebetuk_szama("eeeaaaeee")
+        elvart = 6
+        self.assertEqual(elvart, aktualis, "Nem jól határozta meg az e betűk számát")
+    def test_feladat02(self):
+        aktualis = feladatok.ebetuk_szama("eabcdfgh")
+        elvart = 1
+        self.assertEqual(elvart, aktualis, "Nem jól határozta meg az e betűk számát")
+    def test_feladat03(self):
+        aktualis = feladatok.ebetuk_szama("abcdfghe")
+        elvart = 1
+        self.assertEqual(elvart, aktualis, "Nem jól határozta meg az e betűk számát")
+    def test_feladat04(self):
+        aktualis = feladatok.ebetuk_szama("abcdfgh12fdsd")
         elvart = 0
-        self.assertEqual(elvart, aktualis, "Szöveg")
+        self.assertEqual(elvart, aktualis, "Nem jól határozta meg az e betűk számát")
+    def test_feladat00(self):
+        aktualis = feladatok.ebetuk_szama("")
+        elvart = 0
+        self.assertEqual(elvart, aktualis, "Nem jól határozta meg az e betűk számát")
